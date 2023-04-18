8f47a54be6c22eedca26217a6ab9d5ec694dd0ae
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-18 21:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-//your JS code here. If required.
 window.addEventListener('resize', function() {
     let width = window.innerWidth;
     let height = window.innerHeight;
-    let sizeInfo = document.getElementById('sizeInfo');
-    sizeInfo.innerHTML = '<h1>Width: ' + width + 'px, Height: ' + height + 'px</h1>';
+    let sizeInfo = document.querySelector('#sizeInfo');
+    sizeInfo.innerHTML = '<h1>Width: ' + width + 'px' + ' Height: ' + height + 'px</h1>';
 });
