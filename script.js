ccb87464965713d39923d4cdbb3a80809f6da5c1
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-19 19:50:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-window.addEventListener('resize',DOMContentLoaded, function() {
+window.addEventListener('resize', function() {
     let width = window.innerWidth;
+    let height = window.innerHeight;
+    let sizeInfo = document.querySelector('#sizeInfo');
+    sizeInfo.innerHTML = '<h1>Width: ' + width + 'px' + ' Height: ' + height + 'px</h1>';
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+	 let width = window.innerWidth;
     let height = window.innerHeight;
     let sizeInfo = document.querySelector('#sizeInfo');
     sizeInfo.innerHTML = '<h1>Width: ' + width + 'px' + ' Height: ' + height + 'px</h1>';
